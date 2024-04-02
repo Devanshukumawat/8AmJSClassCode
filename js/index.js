1e98642ed56477dafaec4b4eec67489d1a98c247
Devanshukumawat <devanshukumawat063@gmail.com>
2024-04-02 09:26:40 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,208 @@
+//Singalline comment
+// console.log("Hello")
+// console.log("Hello Class")
+
+
+//Multiline comments
+/* console.log("Hello Good morning class")
+console.log("hi")
+console.log("Hello Good morning class")
+console.log("hi")
+console.log("Hello Good morning class")
+console.log("hi") */
+
+
+//JavaScript Variables -> Variables are containers for storing values.
+// var let const
+
+// var firstName = "Devanshu"
+// console.log("UserName :- "+firstName)
+
+// let lastName = "Kumawat"
+// console.log("Last name :- "+lastName)
+
+// const age = 23
+// console.log("Age :- "+age)
+
+// var
+// var firstName = "Devanshu"
+// var firstName = "Manny"
+//     firstName = "Kizie"
+
+// console.log(firstName)
+
+// {
+//     var firstName = "Dev"
+// }
+// console.log(firstName)
+
+
+//let
+//  let lastName = "Kumawat"
+// //  let lastName = "Sharma"
+// lastName = "Sharma"
+// console.log(lastName)
+
+// {
+//     let age = 16
+
+// }
+// console.log(age)
+
+
+//const
+// const age = 23
+// // const age = 18
+// age = 18
+// console.log(age)
+
+// {
+//     const age = 18
+// }
+
+// console.log(age)
+
+
+//String 
+// let myText = "" '' ``
+
+// let myText = "Kal ho na ho"
+
+// console.log(myText)
+// console.log(typeof(myText))
+
+// let secondText = 'Phir mile ge chal te chal te'
+
+// console.log(secondText)
+// console.log(typeof(secondText))
+
+// let thirdText = `Koi mil gya`
+// console.log(thirdText)
+// console.log(typeof(thirdText))
+
+
+
+// let firstName = "Manny"
+
+// let last_name =  ""
+
+// let FirstName = "kizie"
+
+// console.log(firstName)
+// console.log(FirstName)
+
+
+// let age = 18
+// let num = "98"
+// console.log(age)
+// console.log(typeof(age))
+// console.log(num)
+// console.log(typeof(num))
+
+// true false
+
+// let isMarried = false
+
+// console.log(isMarried)
+// console.log(typeof(isMarried))
+
+// let age;
+// age = 18
+// console.log(age)
+
+// Array 
+// let myArray = ["Devanshu","Swastik","Manny",88,67,true,false]
+//index/key           0           1        2     3  4   5    6
+
+// console.log(typeof(myArray))
+// console.log(myArray)
+
+// let myValue = myArray[6]
+// console.log(myValue)
+
+
+// let myArray  = new Array("Devanshu","Manny",true,78)
+// console.log(myArray)
+// console.log(typeof(myArray))
+// let myValue = myArray[1]
+// console.log(myValue)
+
+
+//object
+
+let Student1 = {
+    firstName:"Devanshu",
+    age:67,
+    id:"8787",
+    roll_no:12,
+    address:{
+        pincode:819819,
+        city:"Jaipur"
+    }
+}
+
+
+let studentCity = Student1.address.city
+let studentPin = Student1.address.pincode
+console.log("Student City :- "+studentCity)
+console.log("Student Pin :- "+studentPin)
+
+// const {firstName,age,roll_no} = Student1
+
+// console.log("First Name :- "+firstName)
+// console.log("Age :- " +age)
+// console.log("Roll_no :- "+roll_no)
+
+// console.log(Student1)
+// console.log(typeof(Student1))
+
+// let studentName = Student1.firstName
+// let studentAge = Student1.age
+
+// console.log( "Student Name :- " +studentName + " Student Age :- "+studentAge)
+
+
+// [
+//     {student1}, 0
+//     {student2}, 1 
+//     {student3}, 2
+// ]
+
+// name[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
