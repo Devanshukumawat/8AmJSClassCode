f77c66533f1b6dcfaa42c32b450c88be957bc298
Devanshukumawat <devanshukumawat063@gmail.com>
2024-04-09 09:15:50 +0530

    ADD FILES

--- js/string.js
+++ js/string.js
@@ -1,4 +1,97 @@
-let str = "Kal ho na ho "
-let sizeOfString = str.length
+// let str = "Kal ho na ho "
+// let str1 = 'Hello Class Good morning'
+// let str2 = `Welcome  ${str}`
+// let sizeOfString = str.length
 
-console.log(sizeOfString)
+// console.log(sizeOfString)
+// console.log(str2)
+
+// let str = "Phir milege chal te chal te Bhaii"
+
+// let cutStr = str.slice(5,11)
+// let cuStr1 = str.slice(28,33)
+// console.log(cutStr)
+// console.log(cuStr1)
+
+// let str = "  Devanshu Sir  "
+
+// let sizeWithspace = str.length
+// console.log(str +" "+sizeWithspace)
+
+// let sizewithoutSpace = str.trim()
+// console.log(sizewithoutSpace + " " + sizewithoutSpace.length)
+
+// let str = "   Hello Class   "
+
+// console.log(str +" Str Length :-  "+ str.length )
+
+// let startSpcae = str.trimStart()
+// let lastSpace = str.trimEnd()
+
+// console.log(startSpcae + " Str Length :- "+startSpcae.length)
+// console.log(lastSpace+" Str Length :- "+lastSpace.length)
+
+
+// let str = "kal ho na ho kal hoga ...."
+
+// let replaceStr = str.replace("Kal","Aaj")
+// console.log(replaceStr)
+
+// let ReplaceAllStr = str.replaceAll("kal","Aaj")
+// console.log(str)
+// console.log(ReplaceAllStr)
+
+
+// let str = "kal ho na ho kal hoga ...."
+
+// let strSplit = str.split("k")
+// console.log(strSplit)
+
+// let str1 = "Kal ho na ho "
+// let str2 = "Phir milege chal te chal te "
+// let str3 = "Hello Class"
+
+// let newStr = str1.concat(str2,str3)
+// console.log(newStr)
+
+// let str = "Ak team me sirf ak hi gunda hota hai or is team ka gunda me hu"
+
+// let indexStr = str.charAt(25)
+
+// console.log(indexStr)
+
+// let str = "Ak team me sirf ak hi gunda hota hai or is team ka gunda me hu"
+
+// let UpperStr = str.toUpperCase()
+// console.log(UpperStr)
+
+// let LowerStr = str.toLowerCase()
+// console.log(LowerStr)
+
+// let str = "Ak team me sirf ak hi gunda hota hai or is team ka gunda me hu"
+
+// let HasValue = str.includes("team")
+// console.log(HasValue)
+
+
+// let str = "Ak team me sirf ak hi gunda hota hai or is team ka gunda me hu"
+
+// let matchStr = str.matchAll("team")
+// console.log(matchStr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
