9523419de6c453286786a29d05c564dfde4c39a1
Devanshukumawat <devanshukumawat063@gmail.com>
2024-04-10 09:26:22 +0530

    add files

---
+++ js/array.js
@@ -0,0 +1,127 @@
+// const myArray = ["devanshu","aman","kizie","Manny"]
+// console.log(myArray)
+// console.log(typeof(myArray))
+
+
+// let myNewArray = new Array("Devanshu",76,true)
+// console.log(myNewArray)
+// console.log(typeof(myNewArray))
+
+
+// const myArray = ["devanshu","aman","kizie","Manny"]
+
+// let value = myArray[2]
+
+// myArray[1] = "swastik"
+
+// console.log(value)
+// console.log(myArray)
+
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// let sizeOfarray = myArray.length
+// console.log(myArray)
+// console.log("Length of Array :- "+sizeOfarray)
+
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// myArray.pop()
+// console.log(myArray)
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// myArray.push("Devanshu Sir")
+// console.log(myArray)
+
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// let removedValue = myArray.shift()
+// console.log(removedValue)
+// console.log(myArray)
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// myArray.unshift("Devanshu Sir")
+// console.log(myArray)
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// let strArray = myArray.toString()
+// console.log(strArray+" Data type :- "+typeof(strArray))
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// let JoinArray = myArray.join(" ")
+// console.log(JoinArray + " Data Type :- "+typeof(JoinArray))
+
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// // myArray.delete("Dev")
+//  let deletedValue = delete myArray[1]
+// console.log(myArray)
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+// const mySecondArray = [12,34,5,true,"Sunny"]
+
+// let newArray = myArray.concat(mySecondArray)
+// console.log(newArray)
+
+// let value = newArray[7]
+// console.log(value)
+
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+
+// let myNewArray = myArray.splice(2,2,"Hello","Welcome")
+
+// console.log(myArray)
+
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+
+// let myNewArray = myArray.slice(3)
+
+// console.log(myArray)
+// console.log(myNewArray)
+
+// let myArray = [
+//     "devanshu",
+//     "daisy",
+//     "kizie",
+//     "manny",
+//     "avinash",
+//     "aashish ji",
+//     "abay",
+//     "akaksha",
+//     "vishnu",
+//     98,78,67
+// ]
+
+// let sortedArray = myArray.sort()
+// console.log(sortedArray)
+
+//loop
+
+// for(let x =1; x<=5; x++){
+//     console.log(x)
+// }
+
+// const myArray = ["Dev","Swastik","Manny","Kizie","Daisy"]
+
+// for(let i=0; i<=myArray.length; i++){
+//     console.log(myArray[i])
+// }
+
+const mySecondArray = ["Dev","Swastik","Manny","Kizie","Daisy",17,89,67,true,false]
+
+for(let i=0; i<mySecondArray.length; i++){
+    console.log(mySecondArray[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
