7bce8dce7f975e9a0488b8d9a6dde58237f8a252
Devanshukumawat <devanshukumawat063@gmail.com>
2024-04-15 08:23:21 +0530

    add some files

---
+++ js/Switch.js
@@ -0,0 +1,27 @@
+// let age = 10
+
+// age==10 ? console.log("Age is 10") 
+// :age>10 ? console.log("Age is grater then 10")
+// :age <10 ? console.log("Age is less then 10")
+// : console.log("Invalid")
+
+
+// let job_type = "Developerasas"
+
+// switch(job_type){
+//         case "Business":
+//             console.log("Business")
+//             break
+        
+//         case "Developer":
+//             console.log("Developer") 
+//             break   
+        
+//         default:
+//             console.log("Default part run")    
+// }
+
+
+setInterval(()=>{
+    console.log("I am a Interval fun")
+  },2000)
--- js/object.js
+++ js/object.js
@@ -1,16 +1,16 @@
-let Product1 = {
-    ProductName:"Samsung_Tv",
-    ProductPrice:"$567",
-    ProductRating:"4.9",
-    Address:{
-        city:"Jaipur",
-        pincode:303930
-    },
+// let Product1 = {
+//     ProductName:"Samsung_Tv",
+//     ProductPrice:"$567",
+//     ProductRating:"4.9",
+//     Address:{
+//         city:"Jaipur",
+//         pincode:303930
+//     },
 
-    allDeatils:function (){
-       console.log(`ProductName :- ${this.ProductName} ProductPrice:- ${this.ProductPrice} ProductRating :- ${this.ProductRating} Address :- ${this.Address.city}`) 
-    }
-}
+//     allDeatils:function (){
+//        console.log(`ProductName :- ${this.ProductName} ProductPrice:- ${this.ProductPrice} ProductRating :- ${this.ProductRating} Address :- ${this.Address.city}`) 
+//     }
+// }
 
 // console.log(Product1.Address.city)
 
@@ -21,4 +21,102 @@ let Product1 = {
 // console.log(ProductRating)
 // console.log(city)
 
-Product1.allDeatils()
+// Product1.allDeatils()
+
+
+// let Product1 = {
+//     ProductName:"Samsung_Tv",
+//     ProductPrice:"$567",
+//     ProductRating:"4.9",
+//     Address:{
+//         city:"Jaipur",
+//         pincode:303930
+//     },
+
+//     allDeatils:function (){
+//        console.log(`ProductName :- ${this.ProductName} ProductPrice:- ${this.ProductPrice} ProductRating :- ${this.ProductRating} Address :- ${this.Address.city}`) 
+//     }
+// }
+
+
+// const {ProductName,ProductRating} = Product1
+
+// console.log(ProductName,ProductRating)
+
+// Product1.allDeatils()
+
+// let age = 18
+
+// age>18 ? console.log("Age is grater then 18")
+// : age==18 ? console.log("Age is equal to 18")
+// : age<18 ? console.log("Age is lessthen 18")
+// :console.log("Invalid input")
+
+// if(age>18){
+//     console.log("Age is grater then 18")
+// }
+// else if(age==18){
+//     console.log("Age is equal to 18")
+// }
+// else if(age<18){
+//     console.log("Age is lessthen 18")
+// }
+// else{
+//     console.log("Invalid input")
+// }
+
+
+
+// let job_type = "Doctasasor"
+
+// switch(job_type){
+//     case "Business":
+//         console.log("Job type is Business")
+//         break
+    
+//     case "Doctor":
+//         console.log("Job type is Doctor")  
+//         break 
+       
+//     case "Driver":
+//         console.log("Job type Driver")
+//         break   
+        
+//     case "Developer":
+//         console.log("Job type is developer")   
+//         break 
+
+//     default:
+//         console.log("Invalid Input")  
+//         break  
+// }
+
+// let num = 10
+// let a  = num.isNaN()
+// console.log(a)
+
+// let a = isNaN("hajahj")
+// console.log(a)
+
+// let  num = 189
+// console.log(num + " "+typeof(num))
+// let str = num.toString()
+// console.log(str + " "+typeof(str))
+
+
+// let num = 5.4
+
+// let a = num.toFixed()
+
+// console.log(a)
+
+
+// let num1 = parseInt("678.12")
+// console.log(num1 + " "+ typeof(num1))
+
+
+// let num2 = parseFloat("12.12")
+// console.log(num2 + " "+ typeof(num2))
+
+// let myDate = new Date()
+// console.log(myDate)
