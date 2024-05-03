6a4a1597f78ca7f0deadd1f657c5cb2c7b62fa41
Devanshukumawat <devanshukumawat063@gmail.com>
2024-05-03 09:04:53 +0530

    add files

--- js/copy.js
+++ js/copy.js
@@ -25,16 +25,16 @@
 
 
 
-let student1 = {
-    name:"Dev",
-    class:"12",
-    age:"18",
-    section:"A",
-    address:{
-        pincode:"61761",
-        city:"jaipur"
-    }
-}
+// let student1 = {
+//     name:"Dev",
+//     class:"12",
+//     age:"18",
+//     section:"A",
+//     address:{
+//         pincode:"61761",
+//         city:"jaipur"
+//     }
+// }
 
 // let student2 = {...student1}
 
@@ -44,9 +44,72 @@ let student1 = {
 // console.log(student2)
 
 
-let student2 = JSON.parse(JSON.stringify(student1))
+// let student2 = JSON.parse(JSON.stringify(student1))
 
-student1.address.city = "Sambhar"
+// student1.address.city = "Sambhar"
 
-console.log(student1)
-console.log(student2)
+// console.log(student1)
+// console.log(student2)
+
+
+// let product1 = {
+//     productName : "Iphone",
+//     productPrice:"$567",
+//     productRating:"4.6",
+//     address:{
+//         city:"Jaipur",
+//         pincode:"5678"
+//     }
+// }
+
+
+// let product2 = product1
+
+// product1.productName = "Samsung"
+
+// let product2 = {...product1}
+
+// product1.productName = "Samsung"
+// product2.productName = "Mi"
+// product1.address.city = "Ajmer"
+
+// console.log(product1)
+// console.log(product2)
+
+// let product2 = JSON.parse(JSON.stringify(product1))
+
+// product1.address.city = "Ajmer"
+
+// console.log(product1)
+// console.log(product2)
+
+// let _ = require ("lodash")
+
+// let product1 = {
+//     productName : "Iphone",
+//     productPrice:"$567",
+//     productRating:"4.6",
+//     address:{
+//         city:"Jaipur",
+//         pincode:"5678"
+//     },
+
+//     allDetails: function (){
+//         console.log( "ProductName :- " +this.productName + " ProductPrice :- " + this.productPrice)
+//     }
+
+// }
+
+// // let product2 = JSON.parse(JSON.stringify(product1))
+
+// let product2 = _.cloneDeep(product1)
+
+// console.log(product1)
+// console.log(product2)
+// product1.allDetails()
+
+
+
+
+
+
